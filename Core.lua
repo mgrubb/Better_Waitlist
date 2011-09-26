0a916b1fcfc3eb9d9ffbf71aa7b61c3cdbf639aa
Michael Grubb <devel@dailyvoid.com>
2011-09-26 15:10:19 -0500

    Change up initialization code, and add semicolons

--- Core.lua
+++ Core.lua
@@ -1,39 +1,37 @@
-Better_Waitlist = LibStub('AceAddon-3.0'):NewAddon('Better_Waitlist',
-													'AceComm-3.0',
-													'AceEvent-3.0',
-													'AceConsole-3.0',
-													'LibWho-2.0')
-
-local addon = Better_Waitlist
-local strsplit, tinsert, InviteUnit, GetChannelList = strsplit, tinsert, InviteUnit, GetChannelList
-local UnitInRaid, GetRaidRosterInfo, SendChatMessage = UnitInRaid, GetRaidRosterInfo, SendChatMessage
-local fmod, removebyval = math.fmod, BetterUtils_removebyval
+-- Get addonname and table provided by blizzard
+local addonName, addon = ...;
+local mixins = { 'AceComm-3.0', 'AceEvent-3.0', 'AceConsole-3.0', 'LibWho-2.0' };
+addon = LibStub('AceAddon-3.0'):NewAddon(addon, 'Better_Waitlist', unpack(mixins));
+Better_Waitlist = addon;
+local strsplit, tinsert, InviteUnit, GetChannelList = strsplit, tinsert, InviteUnit, GetChannelList;
+local UnitInRaid, GetRaidRosterInfo, SendChatMessage = UnitInRaid, GetRaidRosterInfo, SendChatMessage;
+local fmod, removebyval = math.fmod, BetterUtils_removebyval;
 
 local data_meta = {
 	__index = function(t,i)
 		for k,v in pairs(t) do
 			if v.name == i then
-				return v
+				return v;
 			end
 		end
-		return nil
+		return nil;
 	end,
-}
+};
 
 local function catenate_to_map(...)
-	local t,k,v,n
-	t = {}
-	n = select('#', ...)
-	assert(fmod(n,2) == 0)
-	for i = 1, n, 2 do
-		k,v = select(i, ...)
-		t[tostring(k)] = v
+	local tab,key,val,len;
+	tab = {};
+	len = select('#', ...);
+	assert(fmod(len,2) == 0);
+	for i = 1, len, 2 do
+		key,val = select(i, ...);
+		tab[tostring(key)] = val;
 	end
-	return t
+	return t;
 end
 
 local function getChannelTable()
-	return catenate_to_map('GUILD', 'Guild', GetChannelList())
+	return catenate_to_map('GUILD', 'Guild', GetChannelList());
 end
 
 local options = {
@@ -41,6 +39,13 @@ local options = {
 	handler = Better_Waitlist,
 	type = 'group',
 	args = {
+		gui = {
+			type = 'execute',
+			name = 'GUI',
+			desc = 'Open the waitlist GUI',
+			guiHidden = true,
+			func = function() addon:CreateGUI() end
+		},
 		sconfig = {
 			type = 'execute',
 			name = 'Standalone',
@@ -169,7 +174,7 @@ local options = {
 			},
 		},
 	},
-}
+};
 
 local defaults = {
 	profile = {
@@ -196,44 +201,44 @@ local defaults = {
 			queue = {},
 		},
 	},
-}
+};
 
 local whisper_handlers = {
 	invite = 'AddToList',
 	add = 'AddToList',
 	accept = 'SendRaidInvite',
 	remove = 'RemoveFromList',
-}
+};
 
 
 function addon:OnInitialize()
-	self.db = LibStub('AceDB-3.0'):New('Better_WaitlistDB', defaults, true)
-	options.args.profile = LibStub('AceDBOptions-3.0'):GetOptionsTable(self.db)
-	LibStub('AceConfig-3.0'):RegisterOptionsTable('Better_Waitlist', options, {'betterwl', 'bwl'})
-	local acd = LibStub('AceConfigDialog-3.0')
-	self.optionsFrame = acd:AddToBlizOptions('Better_Waitlist', nil, nil, 'general')
-	acd:AddToBlizOptions('Better_Waitlist', options.args.delays.name, 'Better_Waitlist', 'delays')
-	acd:AddToBlizOptions('Better_Waitlist', options.args.messages.name, 'Better_Waitlist', 'messages')
-	acd:AddToBlizOptions('Better_Waitlist', options.args.autoinvite.name, 'Better_Waitlist', 'autoinvite')
-	acd:AddToBlizOptions('Better_Waitlist', options.args.profile.name, 'Better_Waitlist', 'profile')
+	self.db = LibStub('AceDB-3.0'):New('Better_WaitlistDB', defaults, true);
+	options.args.profile = LibStub('AceDBOptions-3.0'):GetOptionsTable(self.db);
+	LibStub('AceConfig-3.0'):RegisterOptionsTable('Better_Waitlist', options, {'betterwl', 'bwl'});
+	local acd = LibStub('AceConfigDialog-3.0');
+	self.optionsFrame = acd:AddToBlizOptions('Better_Waitlist', nil, nil, 'general');
+	acd:AddToBlizOptions('Better_Waitlist', options.args.delays.name, 'Better_Waitlist', 'delays');
+	acd:AddToBlizOptions('Better_Waitlist', options.args.messages.name, 'Better_Waitlist', 'messages');
+	acd:AddToBlizOptions('Better_Waitlist', options.args.autoinvite.name, 'Better_Waitlist', 'autoinvite');
+	acd:AddToBlizOptions('Better_Waitlist', options.args.profile.name, 'Better_Waitlist', 'profile');
 
-	setmetatable(self.db.factionrealm.wlist.list, data_meta)
+	setmetatable(self.db.factionrealm.wlist.list, data_meta);
 	-- Register events
-	self:RegisterComm('BetterWaitlist')
-	self:RegisterEvent('CHAT_MSG_WHISPER')
-	self:RegisterEvent('RAID_ROSTER_UPDATE')
-	self:RegisterMessage('PLAYER_ADDED_TO_WAITLIST')
+	self:RegisterComm('BetterWaitlist');
+	self:RegisterEvent('CHAT_MSG_WHISPER');
+	self:RegisterEvent('RAID_ROSTER_UPDATE');
+	self:RegisterMessage('PLAYER_ADDED_TO_WAITLIST');
 end
 
 function addon:OnEnable()
 	-- If we are in a raid and the wait list is active when enabled then assume
 	-- that this is from a reload/dc and keep the current list.
 	if not UnitInRaid('player') and not self:IsActive() then
-		wipe(self.db.factionrealm.wlist.list)
-		wipe(self.db.factionrealm.wlist.queue)
-		setmetatable(self.db.factionrealm.wlist.list, data_meta)
+		wipe(self.db.factionrealm.wlist.list);
+		wipe(self.db.factionrealm.wlist.queue);
+		setmetatable(self.db.factionrealm.wlist.list, data_meta);
 	else
-		self:StartWaitlist()
+		self:StartWaitlist();
 	end
 end
 
@@ -244,77 +249,77 @@ function addon:OnCommReceived()
 end
 
 function addon:SetActive(val)
-	self.db.factionrealm.wlist.active = val
-	return val
+	self.db.factionrealm.wlist.active = val;
+	return val;
 end
 
 function addon:StartWaitlist()
 	if self:IsActive() then
-		print("[BWL] Waitlist already started.")
-		return nil
+		print("[BWL] Waitlist already started.");
+		return nil;
 	end
-	self:SetActive(true)
-	local config = self.db.profile
+	self:SetActive(true);
+	local config = self.db.profile;
 end
 
 function addon:StopWaitlist()
-	self:SetActive(false)
-	wipe(self.db.factionrealm.wlist.list)
-	wipe(self.db.factionrealm.wlist.queue)
-	print("[BWL] Waitlist has been stopped")
+	self:SetActive(false);
+	wipe(self.db.factionrealm.wlist.list);
+	wipe(self.db.factionrealm.wlist.queue);
+	print("[BWL] Waitlist has been stopped");
 end
 
 function addon:IsActive()
-	return self.db.factionrealm.wlist.active
+	return self.db.factionrealm.wlist.active;
 end
 
 function addon:SendWhisper(recipient, msg, ...)
-	SendChatMessage("[BWL] " .. format(msg, ...), 'WHISPER', nil, recipient)
+	SendChatMessage("[BWL] " .. format(msg, ...), 'WHISPER', nil, recipient);
 end
 
 function addon:GetPlayerData(player)
-	local _, queue, list = self:GetWaitlist()
-	return list[player]
+	local _, queue, list = self:GetWaitlist();
+	return list[player];
 end
 
 function addon:PlayerInRaidOrList(player)
-	local _, queue = self:GetWaitlist()
+	local _, queue = self:GetWaitlist();
 	if UnitInRaid(player) or self:GetPlayerData(player) or queue[player] then
-		return true
+		return true;
 	end
-	return false
+	return false;
 end
 
 function addon:GetWaitlist()
-	return self.db.factionrealm.wlist, self.db.factionrealm.wlist.queue, self.db.factionrealm.wlist.list
+	return self.db.factionrealm.wlist, self.db.factionrealm.wlist.queue, self.db.factionrealm.wlist.list;
 end
 
 function addon:UserInfoAvailable(data, time)
 	if not self:IsActive() then
-		return nil
+		return nil;
 	end
-	local _, queue = self:GetWaitlist()
+	local _, queue = self:GetWaitlist();
 	for n,_ in pairs(queue) do
 		if data and (data.Name == n) then
-			queue[n] = nil
-			self:AddPlayerToWaitlist(n, data)
+			queue[n] = nil;
+			self:AddPlayerToWaitlist(n, data);
 		end
 	end
 end
 
 function addon:AddPlayerToWaitlist(player, data)
-	local wlist, queue, list = self:GetWaitlist()
+	local wlist, queue, list = self:GetWaitlist();
 	if not data then return nil end
 	if queue[player] then
-		queue[player] = nil
+		queue[player] = nil;
 	end
 
 	if not data.Name then
-		data.Name = player
+		data.Name = player;
 	end
 
 	if self:PlayerInRaidOrList(player) then
-		return nil
+		return nil;
 	end
 
 	local ldata = {
@@ -332,104 +337,104 @@ function addon:AddPlayerToWaitlist(player, data)
 				value = data.Level
 			}, -- level column
 		},
-	}
-	tinsert(list, ldata)
-	self:SendMessage('PLAYER_ADDED_TO_WAITLIST', player, ldata)
-	return true
+	};
+	tinsert(list, ldata);
+	self:SendMessage('PLAYER_ADDED_TO_WAITLIST', player, ldata);
+	return true;
 end
 
 function addon:PLAYER_ADDED_TO_WAITLIST(player, data)
-	self:SendWhisper(player, 'You have been added to the waitlist.')
+	self:SendWhisper(player, 'You have been added to the waitlist.');
 end
 
 function addon:AddToList(player, password, ...)
 	if not self:IsActive() then
 		self:SendWhisper(sender, 'The waitlist is currently inactive.');
-		return nil
+		return nil;
 	end
-	local config = self.db.profile
+	local config = self.db.profile;
 	if self:PlayerInRaidOrList(player) then
-		addon:SendWhisper(player, "You are already on the list or in the raid")
-		return nil
+		addon:SendWhisper(player, "You are already on the list or in the raid");
+		return nil;
 	end
 
 	if config.autoInvite.mode then
 		if config.autoInvite.mode == 'enabled' or
 			(config.autoInvite.mode == 'onlypw' and config.autoInvite.password == password) then
-			self:SendRaidInvite(player, true)
-			return nil
+			self:SendRaidInvite(player, true);
+			return nil;
 		end
 	end
 
-	local data = self:UserInfo(player, {timeout = 20, callback = 'UserInfoAvailable', handler = self})
+	local data = self:UserInfo(player, {timeout = 20, callback = 'UserInfoAvailable', handler = self});
 	if data then
-		self:AddPlayerToWaitlist(player, data)
+		self:AddPlayerToWaitlist(player, data);
 	end
 end
 
 function addon:RemovePlayerFromLists(player)
-	local _, queue, list = self:GetWaitlist()
-	queue[player] = nil
-	removebyval(list, list[player])
+	local _, queue, list = self:GetWaitlist();
+	queue[player] = nil;
+	removebyval(list, list[player]);
 end
 
 function addon:AcceptPlayer(player)
-	local data = self:GetPlayerData()
+	local data = self:GetPlayerData();
 	if not self:IsActive() or not data then
-		return nil
+		return nil;
 	end
 
-	data.accepted = true
-	self:SendWhisper('You have been invited to the raid.  Reply with the word \'accept\' to get your invite')
-	return true
+	data.accepted = true;
+	self:SendWhisper('You have been invited to the raid.  Reply with the word \'accept\' to get your invite');
+	return true;
 end
 
 function addon:SendRaidInvite(player, auto)
-	local data = self:GetPlayerData()
+	local data = self:GetPlayerData();
 	if auto then
-		self:RemovePlayerFromLists(player)
-		InviteUnit(player)
-		return true
+		self:RemovePlayerFromLists(player);
+		InviteUnit(player);
+		return true;
 	end
 
 	if not data then
-		self:SendWhisper('Could not find data for you, add yourself to the waitlist again')
-		return false
+		self:SendWhisper('Could not find data for you, add yourself to the waitlist again');
+		return false;
 	end
 
 	if not data.accepted then
-		self:SendWhisper('You haven\'t recieved and invite yet.')
-		return false
+		self:SendWhisper('You haven\'t recieved and invite yet.');
+		return false;
 	end
 
-	self:RemovePlayerFromLists(player)
-	InviteUnit(player)
-	return true
+	self:RemovePlayerFromLists(player);
+	InviteUnit(player);
+	return true;
 end
 
 function addon:RAID_ROSTER_UPDATE()
 	if not self:IsActive() then
-		return nil
+		return nil;
 	end
 
-	local _, queue, list = self:GetWaitlist()
+	local _, queue, list = self:GetWaitlist();
 
 	for i=1,40 do
-		local name = GetRaidRosterInfo(i)
-		queue[name] = nil
-		list[name] = nil
+		local name = GetRaidRosterInfo(i);
+		queue[name] = nil;
+		list[name] = nil;
 	end
 end
 
 function addon:CHAT_MSG_WHISPER(event, msg, sender)
 	if not self:IsActive() then
-		return nil
+		return nil;
 	end
 
-	local command, args = strsplit(" ", msg)
+	local command, args = strsplit(" ", msg);
 	if whisper_handlers[command] then
-		local handler = addon[whisper_handlers[command]]
-		handler(self, sender, strsplit(" ", args))
+		local handler = addon[whisper_handlers[command]];
+		handler(self, sender, strsplit(" ", args));
 	end
 end
 
